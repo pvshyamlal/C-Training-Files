569885a7cd325243b1166863048b0a32ef258800
pvshyamlal <pvshyamlal4@gmail.com>
2024-07-16 14:35:13 +0530

    ds

--- Day13/Task2.cs
+++ Day13/Task2.cs
@@ -9,23 +9,51 @@ the main thread should wait for all runner threads to finish before announcing t
 using System;
 using System.Threading;
 class Task2{
-    static void Run(object runnerName){
-        string name = (string)runnerName;
+    static void Run(runner1){
+        string name = runner1;
         Console.WriteLine(name+" started running.");
         for (int lap = 1; lap <= 5; lap++){
             Console.WriteLine(name+" completed lap"+ lap);
             Thread.Sleep(1000);
             if (lap < 5){
                 Console.WriteLine(name+" taking a short break.");
-                Thread.Sleep(500);
+                Thread.Sleep(3000);
+            }
+        }
+        Console.WriteLine(name+" finished running.");
+    }
+
+    static void Run(runner2){
+        string name = runner2;
+        Console.WriteLine(name+" started running.");
+        for (int lap = 1; lap <= 5; lap++){
+            Console.WriteLine(name+" completed lap"+ lap);
+            Thread.Sleep(1000);
+            if (lap < 5){
+                Console.WriteLine(name+" taking a short break.");
+                Thread.Sleep(1000);
+            }
+        }
+        Console.WriteLine(name+" finished running.");
+    }
+
+    static void Run(runner3){
+        string name = runner3;
+        Console.WriteLine(name+" started running.");
+        for (int lap = 1; lap <= 5; lap++){
+            Console.WriteLine(name+" completed lap"+ lap);
+            Thread.Sleep(1000);
+            if (lap < 5){
+                Console.WriteLine(name+" taking a short break.");
+                Thread.Sleep(4000);
             }
         }
         Console.WriteLine(name+" finished running.");
     }
     static void Main(){
-        Thread runner1 = new Thread(Run);
-        Thread runner2 = new Thread(Run);
-        Thread runner3 = new Thread(Run);
+        Thread runner1 = new Thread(Runner1);
+        Thread runner2 = new Thread(Runner2);
+        Thread runner3 = new Thread(Runner3);
         runner1.Start("Runner 1");
         runner2.Start("Runner 2");
         runner3.Start("Runner 3");
