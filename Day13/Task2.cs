e51521636be6012ae9bbb22a62efb031337e7cf3
pvshyamlal <pvshyamlal4@gmail.com>
2024-07-16 14:24:38 +0530

    Create Task2.cs

---
+++ Day13/Task2.cs
@@ -0,0 +1,9 @@
+/*
+Race Track Simulation
+Create a program that simulates a race between three runners. Each runner is represented by a separate 
+thread. The race consists of 5 laps, and after each lap, the runner needs to take a short break. Use 
+\the Thread.Sleep() method to simulate the time taken for each lap and break. At the end of the race, 
+the main thread should wait for all runner threads to finish before announcing the results.
+*/
+
+using System;
