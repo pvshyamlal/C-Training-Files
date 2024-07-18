285f0fddd77955a1658cb2d1271ee14261308f5b
pvshyamlal <pvshyamlal4@gmail.com>
2024-07-18 14:08:40 +0530

    C#

--- Day15/demo.cs
+++ Day15/demo.cs
@@ -1,19 +1,41 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-class demo{
+
+class C1{
     public static void Main(string[] args){
-        Collection<string> mycall = new Collection<string>();
-        mycall.Add("Hello");
-        mycall.Add("Hi:");
-        mycall.Add("Hey");
-        string[] s1 = new string[mycall.Count];
-        mycall.CopyTo(s1,0); // s1 = {"Hello", "Hi:", "Hey"}
-        Console.WriteLine(s1[0]);
-        mycall.Insert(1, "Hola");
-        mycall.Remove("Hey");
-        foreach (string greeting in mycall)
+        Collection<string> i1=new Collection<string>();
+        i1.Add("A");
+        i1.Add("B");
+        i1.Add("C");
+        Console.WriteLine("No of Elements"+i1.Count);
+        var enumerator =i1.GetEnumerator();
+        while(enumerator.MoveNext()){
+            Console.WriteLine(enumerator.Current);
+        }
+       enumerator.Reset();
+        Console.WriteLine("No of Elements"+i1.Count);
+        while(enumerator.MoveNext()){
+            Console.WriteLine(enumerator.Current);
+        }
+        //foreach
+         Console.WriteLine("For Each");
+        foreach(var a in i1){
+            Console.WriteLine(a);
+        }
+        //ienumerable
+         Console.WriteLine("Enumerable");
+        IEnumerable<string> i2 = i1;
+        foreach(var a in i2){
+            Console.WriteLine(a);
+        }
+        //list enumerator
+        Console.WriteLine("List Enumerator");
+        List<string> list = new List<string>(i1);
+        var listEnumerator = list.GetEnumerator();
+        while (listEnumerator.MoveNext())
         {
-            Console.WriteLine(greeting);
+            Console.WriteLine(listEnumerator.Current);
         }
     }
 }
